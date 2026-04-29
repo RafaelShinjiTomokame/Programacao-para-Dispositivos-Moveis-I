38de052863418c9b9c27409029b52cb7e4307c0b
Rafael_Tomokame <rafaelshinjitomokame@gmail.com>
2026-04-29 19:50:03 -0300

    Delete AppScholar directory

--- AppScholar/backend/routes/boletimRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const boletimController = require('../controllers/boletimController');
-
-router.get('/boletim/:matricula', boletimController.obterBoletim);
-
-module.exports = router;
--- AppScholar/frontend/src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // ⚠️ Troque pelo IP da sua máquina na rede
-  timeout: 10000,
-});
-
-export default api;
