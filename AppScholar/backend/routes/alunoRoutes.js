d80f9bb7adf87af838612519cffccb068aa03da5
Rafael_Tomokame <rafaelshinjitomokame@gmail.com>
2026-04-29 19:18:15 -0300

    Delete AppScholar directory

--- AppScholar/backend/routes/alunoRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middlewares/auth');
-const alunoController = require('../controllers/alunoController');
-
-router.post('/alunos', auth, alunoController.criar);
-router.get('/alunos', auth, alunoController.listar);
-
-module.exports = router;
--- AppScholar/backend/routes/disciplinaRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middlewares/auth');
-const disciplinaController = require('../controllers/disciplinaController');
-
-router.post('/disciplinas', auth, disciplinaController.criar);
-router.get('/disciplinas', auth, disciplinaController.listar);
-
-module.exports = router;
